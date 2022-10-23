9b38a0b8aa81ce99551703e7c6d2a235f237b2ba
harioms1522 <harioms1522@gmail.com>
2022-10-23 20:45:51 +0530

    Error handling

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // core modules
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError")
+
+// controllers
+const globalErrorHandler = require("./controllers/errorController")
 
 // Custom Modules
 const tourRouter = require("./routes/tourRoutes");
