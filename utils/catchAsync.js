c7b7a3f7aa29e08bc1554c158bff6cc291cec53e
harioms1522 <harioms1522@gmail.com>
2022-10-23 20:18:52 +0530

    Error handling

---
+++ utils/catchAsync.js
@@ -0,0 +1,12 @@
+/*
+    It takes an async function 
+
+    Takes a function then returns a function like middleware to be called by route
+    and this returned function then called the parameter function and the catch is used on this and transferred to the error handling route
+*/
+
+module.exports = fn => {
+    return (req, res, next) => {
+      fn(req, res, next).catch(next);
+    };
+  };
